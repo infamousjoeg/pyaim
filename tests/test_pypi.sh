1da82e1b014d94a023bf50fc76dcb4a7336cf7fc
Joe Garcia <joe@joe-garcia.com>
2019-03-11 14:39:58 -0400

    pruned down tests to only relevant

--- tests/test_pypi.sh
+++ tests/test_pypi.sh
@@ -2,11 +2,7 @@
 #
 # Upload Package to test.pypi.org
 
-echo "Upgrading setuptools and wheel..."
-python3 -m pip install --upgrade setuptools wheel >/dev/null
 echo "Running sdist and bdist_wheel from setup.py..."
-python3 setup.py sdist bdist_wheel >/dev/null
-echo "Upgrading Twine..."
-python3 -m pip install --upgrade twine >/dev/null
+python3 setup.py sdist bdist_wheel
 echo "Uploading to test.pypi.org via Twine..."
 python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
