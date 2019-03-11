f286361ed3665a9ea596e233dc16b2ad54d2eb64
Joe Garcia <joe@joe-garcia.com>
2019-03-11 13:11:10 -0400

    Added in optional metadata

--- setup.py
+++ setup.py
@@ -14,10 +14,18 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/infamousjoeg/pyaim",
+    license="MIT",
+    platform="OS Independent",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ]
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    keywords='cyberark aim aam identity access security pam pim pas',
+    python_requires='>=3.*.*, <4'
+    project_urls={
+        'Bug Reports': 'https://github.com/infamousjoeg/pyaim/issues/new?assignees=&labels=&template=bug_report.md&title=',
+        'Feature Requests': 'https://github.com/infamousjoeg/pyaim/issues/new?assignees=&labels=&template=feature_request.md&title='
+        'Source': 'https://github.com/infamousjoeg/pyaim'
+    }
 )
