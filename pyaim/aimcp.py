9f1e5da59f2370e4e768b3013176671fc131fa8a
Joe Garcia, CISSP <infamousjoeg@users.noreply.github.com>
2019-03-15 16:27:51 -0400

    Fixed review comments from PR #1

--- pyaim/aimcp.py
+++ pyaim/aimcp.py
@@ -28,38 +28,33 @@ class CLIPasswordSDK(object):
             raise Exception('Cannot detect OS', 'Your platform is unrecognizable. Please use Linux or Windows.')
 
 
-    def GetPassword(self, appid=None, safe=None, obj=None):
-        # Check appid is declared
-        if appid is None:
-            raise Exception('No Application ID', 'Please declare a valid Application ID.')
-        # Check safe is declared
-        elif safe is None:
-            raise Exception('No Safe Name', 'Please declare a valid Safe name.')
-        # Check object name is declared
-        elif obj is None:
-            raise Exception('No Object Name', 'Please declare a valid Object name.')
-        else:
-            try:
-                response, err = Popen(
-                    [
-                        '{}'.format(self._cli_path),
-                        'GetPassword',
-                        '{}p'.format(self.sep), 'AppDescs.AppID={}'.format(appid),
-                        '{}p'.format(self.sep), 'Query=safe={safe};folder=Root;object={obj}'.format(safe=safe,obj=obj),
-                        '{}o'.format(self.sep), 'PassProps.UserName,Password,PassProps.Address,PassProps.Port,PasswordChangeInProcess'
-                    ],
-                    stdout=PIPE,
-                    stderr=PIPE
-                ).communicate()
+    def GetPassword(self, appid=None, safe=None, objectName=None):
+        var_list = [appid, safe, objectName]
+        for var in var_list:
+            if var is None:
+                raise Exception('No {}'.format(var), 'Please declare a valid {}.'.format(var))
 
-                if err:
-                    raise Exception(err.decode('UTF-8').strip())
-            except:
-                print(err.decode('UTF-8').strip())
-                exit()
-            else:
-                key_list = ['Username','Password','Address','Port','PasswordChangeInProcess']
-                val_list = response.decode('UTF-8').strip().split(',')
-                zip_list = zip(key_list,val_list)
-                ret_response = dict(zip_list)
-                return ret_response
+        try:
+            response, err = Popen(
+                [
+                    self._cli_path,
+                    'GetPassword',
+                    self.sep + 'p', 'AppDescs.AppID={}'.format(appid),
+                    self.sep + 'p', 'Query=safe={safe};folder=Root;object={objectName}'.format(safe=safe,objectName=objectName),
+                    self.sep + 'o', 'PassProps.UserName,Password,PassProps.Address,PassProps.Port,PasswordChangeInProcess'
+                ],
+                stdout=PIPE,
+                stderr=PIPE
+            ).communicate()
+
+            if err:
+                raise Exception(err.decode('UTF-8').strip())
+        except e as Exception:
+            print(e)
+            exit()
+            
+        key_list = ['Username','Password','Address','Port','PasswordChangeInProcess']
+        val_list = response.decode('UTF-8').strip().split(',')
+        zip_list = zip(key_list,val_list)
+        ret_response = dict(zip_list)
+        return ret_response
