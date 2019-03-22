22449c17f547445844af28d5b8b71a1b37918f93
Joe Garcia <joe@joe-garcia.com>
2019-03-22 15:43:10 -0400

    Changes per PR #8 Review

--- pyaim/aimccp.py
+++ pyaim/aimccp.py
@@ -10,24 +10,29 @@ class CCPPasswordREST(object):
     def __init__(self, base_uri, verify=True):
 
         # Declare Init Variables
-        self._base_uri = base_uri.rstrip('/')   # Example: https://pvwa.cyberarkexample.com
-        
+        self._base_uri = base_uri.rstrip('/')                # Example: https://pvwa.cyberarkexample.com
+        self._context = ssl.create_default_context()
+        self._headers = {'Content-Type': 'application/json'} # Build Header for GET Request
+
         if verify is False:
-            self._context = ssl.create_default_context()
             self._context.check_hostname = False
             self._context.verify_mode = ssl.CERT_NONE
-        else:
-            self._context = ssl.create_default_context()
 
 
     # Checks that the AIM Web Service is available
     def check_service(self):
         try:
-            conn = urllib.request.urlopen(self._base_uri + '/AIMWebService/v1.1/aim.asmx', context=self._context)
-            status_code = conn.getcode()
-            conn.close()
+            with urllib.request.urlopen(
+                self._base_uri + '/AIMWebService/v1.1/aim.asmx',
+                context=self._context
+            ) as conn:
+                status_code = conn.getcode()
+
+            if status_code != 200:
+                raise Exception('AIMWebService Not Found')
+
         except Exception as e:
-            return 'AIMWebService Not Found. Exception: {}'.format(e)
+            raise Exception(e)
             exit()
 
         return 'AIMWebService Found. Status Code: {}'.format(status_code)
@@ -40,21 +45,20 @@ class CCPPasswordREST(object):
             if var is None:
                 raise Exception('No {}'.format(var), 'Please declare a valid {}.'.format(var))
 
+        # Urlify parameters for GET Request
+        params = urllib.parse.urlencode({'appid': appid, 'folder': 'root', 'safe': safe, 'object': objectName})
+        # Build URL for GET Request
+        url = '{}/AIMWebService/api/Accounts?{}'.format(self._base_uri,params)
+
         try:
-            # Urlify parameters for GET Request
-            params = urllib.parse.urlencode({'appid': appid, 'folder': 'root', 'safe': safe, 'object': objectName})
-            # Build Header for GET Request
-            headers = {'Content-Type': 'application/json'}
-            # Build URL for GET Request
-            url = self._base_uri + '/AIMWebService/api/Accounts?{}'.format(params)
-            request = urllib.request.Request(url, headers=headers)
+            request = urllib.request.Request(url, headers=self._headers)
             response = urllib.request.urlopen(request, context=self._context)
             resp_data = response.read()
 
         # Capture Any Exceptions that Occur
         except Exception as e:
             # Print Exception Details and Exit
-            return(e)
+            raise Exception(e)
             exit()
         
         # Deal with Python dict for return variable
