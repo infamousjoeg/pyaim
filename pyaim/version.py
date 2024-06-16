0f4a82fb94b53743fed738873f5e87b63cdcb9e5
Joe Garcia <infamousjoeg@users.noreply.github.com>
2024-06-16 14:17:03 +0000

    Release v1.5.2

--- pyaim/version.py
+++ pyaim/version.py
@@ -4,6 +4,8 @@
 Version module
 This module contains the version information about the project that
 is intended to be reused in multiple places.
+
+Changing triggers automatic release of a new version.
 """
 
 __version__ = '1.5.2'
