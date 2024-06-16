8c1f41fa2dc51f86c63ad235b9667d5209c3c381
Joe Garcia <infamousjoeg@users.noreply.github.com>
2024-06-16 14:29:17 +0000

    Fix version parsing

--- pyaim/version.py
+++ pyaim/version.py
@@ -5,7 +5,7 @@ Version module
 This module contains the version information about the project that
 is intended to be reused in multiple places.
 
-Changing triggers automatic release of a new version.
+Changing triggers automatic release of a new version. 
 """
 
 __version__ = '1.5.2'
